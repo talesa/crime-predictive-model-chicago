6974690f57cf42b46a90fe9199ddad957acd184a
Lorenzo Martinico <lzmartinico@gmail.com>
2016-02-18 22:32:32 +0000

    Add automatic select

--- webapp.py
+++ webapp.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template
 
 app = Flask('crime')
 
-maps = [{'name': 'past'}, {'name': 'battery'}]
+maps = [{'name': 'past', 'url': 'past.html'}, {'name': 'battery', 'url': 'battery.html'}]
 
 @app.route('/')
 @app.route('/past.html')
 def past_data():
-    return render_template('showmap.html', title="Chicago crime map", maps=maps, current='past')
+    return render_template('showmap.html', title="Chicago crime map", maps=maps, current="past.html")
 
 @app.route('/battery.html')
 def battery():
-    return render_template('showmap.html', title="Chicago crime map", maps=maps, current='battery')
+    return render_template('showmap.html', title="Chicago crime map", maps=maps, current="battery.html")
 
 if __name__ == '__main__':
     app.run(debug=True)

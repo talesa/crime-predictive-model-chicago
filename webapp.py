e231d181252346b7a18a41dc8eaa6ee61f570cd1
Lorenzo Martinico <lzmartinico@gmail.com>
2016-02-18 17:33:53 +0000

    Add map switching options (hardcoded)

--- webapp.py
+++ webapp.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 
+
 app = Flask('crime')
 
+maps = [{'name': 'past'}, {'name': 'battery'}]
 
 @app.route('/')
-def hello_world():
-    return render_template('showmap.html', title="Chicago crime map")
+@app.route('/past.html')
+def past_data():
+    return render_template('showmap.html', title="Chicago crime map", maps=maps, current='past')
+
+@app.route('/battery.html')
+def battery():
+    return render_template('showmap.html', title="Chicago crime map", maps=maps, current='battery')
 
 if __name__ == '__main__':
     app.run(debug=True)

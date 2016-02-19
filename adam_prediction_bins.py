09de799146b15d74e47044eb98932c8842f1b3c2
talesa <adam.gol1993@gmail.com>
2016-02-19 08:22:07 +0000

    simple, weekdays prediction working, bins prediction started

--- adam_prediction_bins.py
+++ adam_prediction_bins.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[49]:
+# In[1]:
 
 get_ipython().magic(u'matplotlib inline')
 import pandas as pd
@@ -9,21 +9,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# #### Everything from adam_simple_prediction 
+
 # In[2]:
 
 crime_chicago = pd.read_pickle('crime_chicago_with_timestamp.pkl')
 
 
 # In[3]:
-
-crime_chicago.head(1)
-
-
-# ### Manipulating data
-
-# Binning the data with the size of the bin equal `bin_size`
-
-# In[4]:
 
 number_of_bins = 70
 no = number_of_bins
@@ -37,13 +30,10 @@ lon_span = crime_chicago['Longitude'].max() - crime_chicago['Longitude'].min()
 lat_step = lat_span / no
 lon_step = lon_span / no
 
-
-# In[5]:
-
 crime_chicago['bin'] =     no*np.floor((crime_chicago['Longitude']-lon_min)/lon_step) +        np.floor((crime_chicago['Latitude'] -lat_min)/lat_step)
 
 
-# In[6]:
+# In[4]:
 
 def getPolygonForBin(bin_no):
     lat, lon = getCornerLatLonForBin(bin_no)
@@ -53,10 +43,6 @@ def getPolygonForBin(bin_no):
             (lat + lat_step, lon + lon_step),
             (lat + lat_step, lon),
         ])
-
-
-# In[7]:
-
 def getPolygonForBinReverse(bin_no):
     lat, lon = getCornerLatLonForBin(bin_no)
     return Polygon([
@@ -67,64 +53,41 @@ def getPolygonForBinReverse(bin_no):
         ])
 
 
-# #### Grouping
+# # THEFTS ONLY
 
-# In[8]:
+# In[24]:
 
-crime_chicago['datetime'].head(1)
+crime_chicago_copy = crime_chicago.copy()
 
 
-# In[9]:
+# In[22]:
+
+crime_chicago['Primary Type'].unique()
+
+
+# In[25]:
+
+crime_chicago = crime_chicago[crime_chicago['Primary Type'] == 'THEFT']
+
+
+# In[26]:
 
 crime_chicago.index = pd.DatetimeIndex(crime_chicago['datetime'])
-
-
-# In[10]:
-
 crime_chicago_count = crime_chicago[['bin', 'datetime']].groupby(['bin', 'datetime']).size().reset_index().rename(columns={0:'count'})
-
-
-# In[11]:
-
-crime_chicago_count['count'].describe()
-
-
-# In[12]:
-
 crime_chicago_count.index = pd.DatetimeIndex(crime_chicago_count['datetime'])
 
 
-# In[13]:
+# Importing weather data
 
-crime_chicago_count.head(1)
-
-
-# ### Importing weather data
-
-# In[14]:
+# In[27]:
 
 weather_chicago = pd.read_csv('WeatherChicago20012016.csv')
 
-
-# In[15]:
-
 def weather_date_to_datetime(date):
-#     print date[0:4], date[6:], date[4:6]
     return pd.datetime(int(date[0:4]), int(date[4:6]), int(date[6:]))
 
-
-# In[16]:
-
 weather_chicago['DATE'] = weather_chicago['DATE'].map(lambda x: weather_date_to_datetime(str(x)))
-
-
-# In[17]:
-
 weather_chicago.index = pd.DatetimeIndex(weather_chicago['DATE'])
-
-
-# In[18]:
-
 tmin_mean = weather_chicago['TMIN'].mean()
 tmax_mean = weather_chicago['TMAX'].mean()
 
@@ -134,129 +97,83 @@ weather_chicago.loc[weather_chicago['PRCP'] == -9999, ['PRCP']] = 0
 weather_chicago.loc[weather_chicago['AWND'] == -9999, ['AWND']] = 0
 
 
-# In[19]:
+# Concatenating crimes and weather  
+# Finding empty records
 
-weather_chicago.describe()
-
-
-# In[20]:
-
-weather_chicago.head(2)
-
-
-# ### Concatenating crimes and weather
-
-# In[21]:
+# In[28]:
 
 crime_chicago_count_ref = crime_chicago[['datetime']].groupby(['datetime']).size().reset_index().rename(columns={0:'count'})
 crime_chicago_count_ref.index = pd.DatetimeIndex(crime_chicago_count_ref['datetime'])
-crime_chicago_count_ref.head(1)
-
-
-# In[22]:
-
 crime_chicago_count_weather = crime_chicago_count_ref.join(weather_chicago[['PRCP', 'TMAX','AWND']])
-
-
-# In[23]:
-
-crime_chicago_count_weather.head(1)
-
-
-# #### Finding empty records
-
-# In[24]:
-
 indeces = pd.isnull(crime_chicago_count_weather).any(1)
 crime_chicago_count_weather.drop(crime_chicago_count_weather.index[indeces], inplace=True)
-len(crime_chicago_count_weather)
 
 
 # ## Prediction
 
-# In[50]:
+# In[29]:
 
-import numpy as np
-import pandas as pd
 import statsmodels.formula.api as smf
 import statsmodels.api as sm
 from scipy.stats.stats import pearsonr
 
 
-# In[26]:
-
-crime_chicago_count_weather.describe()
-
-
-# In[27]:
-
-crime_chicago_count_weather['ones'] = crime_chicago_count_weather.loc[:,['count']]
-crime_chicago_count_weather.loc[:,['ones']] = 1
-dta = crime_chicago_count_weather
-
-
-# In[28]:
-
-res = smf.glm('count~TMAX', data=dta, 
-                family=sm.families.Poisson(link=sm.families.links.log)).fit()
-res.summary()
-
-
-# In[53]:
-
-res = smf.glm('count~TMAX', data=dta, 
-                family=sm.families.Poisson(link=sm.families.links.log)).fit()
-res.summary()
-
-
-# In[29]:
-
-nobs = res.nobs
-yhat = res.mu
-
-
 # In[30]:
 
+crime_chicago_count_weather['weekday'] = crime_chicago_count_weather.index.weekday
+
+
+# In[31]:
+
+weekday_dummies = pd.get_dummies(crime_chicago_count_weather.index.weekday, prefix='weekday')
+weekday_dummies.index = crime_chicago_count_weather.index
+crime_chicago_count_weather_weekdays = crime_chicago_count_weather.join(weekday_dummies)
+
+
+# In[52]:
+
 from statsmodels.graphics.api import abline_plot
+def fit_summary_plot(formula_temp, dta):
+    print(formula_temp)
+    res_temp = smf.glm(formula_temp, data=dta, family=sm.families.Poisson(link=sm.families.links.log)).fit()
+    print(res_temp.summary())
+    nobs = res.nobs
+    yhat = res.mu
+    y = crime_chicago_count_weather_weekdays.loc[:,'count']
+
+    fig, ax = plt.subplots()
+    ax.scatter(yhat, y)
+    line_fit = sm.OLS(y, sm.add_constant(yhat, prepend=True)).fit()
+    abline_plot(model_results=line_fit, ax=ax)
+
+    plt.axis([0, 500, 0, 500])
+
+    ax.set_title('Model Fit Plot')
+    ax.set_ylabel('Observed values')
+    ax.set_xlabel('Fitted values')
+    
+    return yhat
 
 
-# In[39]:
+# In[55]:
 
-y = crime_chicago_count_weather.loc[:,'count']
-
-
-# In[56]:
-
-yhat[:] = crime_chicago_count_weather['count'].mean()
+formula = 'count ~ TMAX + weekday_0 + weekday_1 + weekday_2 + weekday_3 + weekday_4 + weekday_5 + weekday_6'
+fitted = fit_summary_plot(formula, crime_chicago_count_weather_weekdays)
 
 
-# In[51]:
+# - different crime types
+# - bins
+# - timescales
+# - only shopping centre we're interested in (by hour?)
 
-fig, ax = plt.subplots()
-ax.scatter(yhat, y)
-line_fit = sm.OLS(y, sm.add_constant(yhat, prepend=True)).fit()
-abline_plot(model_results=line_fit, ax=ax)
+# ## 1
+# - it's provided df with counted crimes
+# - appends weather
+# - finds empty records and drops them
+# - predicts and plots
 
-plt.axis([0, 2000, 0, 2000])
+# ## 2
+# - bins crimes within given two corners
 
-ax.set_title('Model Fit Plot')
-ax.set_ylabel('Observed values')
-ax.set_xlabel('Fitted values');
-
-
-# In[60]:
-
-fig, ax = plt.subplots()
-ax.scatter(yhat, y)
-line_fit = sm.OLS(y, sm.add_constant(yhat, prepend=True)).fit()
-# abline_plot(model_results=1, ax=ax)
-
-plt.axis([0, 2000, 0, 2000])
-
-ax.set_title('Model Fit Plot')
-ax.set_ylabel('Observed values')
-ax.set_xlabel('Fitted values');
-
-
-# - discuss dependence of crime on temperature
-# 
+# ## 3
+# - chooses a crime type or provides all

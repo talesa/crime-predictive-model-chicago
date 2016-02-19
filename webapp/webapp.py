204a64a2c59c00ca69349dd40c7d8a39d0982c5e
Lorenzo Martinico <lzmartinico@gmail.com>
2016-02-19 10:21:04 +0000

    Extracted fetch weather function

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,16 +1,5 @@
 from flask import Flask, render_template
 
-import requests
-
-r = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Chicago&mode=json&units=metric&cnt=7&appid=44db6a862fba0b067b1930da0d769e98')
-r.headers
-data = r.json()["list"][1]
-weather_tomorrow = {'temp': data['temp']['max'], 'rain': 0}
-if('rain' in data.keys()):
-        weather_tomorrow['rain'] = data['rain']['3h']*10
-
-#TODO: save map as tomorrow.html
-
 app = Flask('crime')
 
 maps = [{'name': 'Historic data', 'url': 'past.html'}, {'name': 'Prediction on tomorrow\'s weather', 'url': 'tomorrow.html'}]
